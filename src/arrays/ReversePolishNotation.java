bc0885aa6ad06860e9e3d71bcb53262e29e79877
Austin Oyugi <austinoyugi@gmail.com>
2024-08-16 04:28:07 +0300

    Added reverse polish notation

---
+++ src/arrays/ReversePolishNotation.java
@@ -0,0 +1,104 @@
+package arrays;
+
+import java.util.*;
+
+/*
+   @author Austin Oyugi
+   @since 15/08/2024
+   @mail austinoyugi@gmail.com
+*/
+public class ReversePolishNotation {
+
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> collectionStack = new Stack<>();
+
+        for (String element: tokens){
+
+            try{
+                Integer operand = Integer.parseInt(element);
+                // Add the element to a stack yet to be calculated
+                collectionStack.push(operand);
+            }catch (Exception ignored){
+
+                // Confirms that the element is operator
+                // Thus we can  start working on the last 2 elements of the collection stack
+
+                // Fill the evaluation stack with the expected calculation
+                // Pushing to a stack also makes sure the order is correct
+                Stack<Integer> evaluationStack = new Stack<>();
+                evaluationStack.push(collectionStack.pop());
+                evaluationStack.push(collectionStack.pop());
+
+                // We only expect 2 elements to evaluate
+                Integer[] calculationValuesInTheCorrectOrder = new Integer[2];
+                int index = 0;
+
+                while (!evaluationStack.isEmpty()){
+                    calculationValuesInTheCorrectOrder[index] = evaluationStack.pop();
+                    index ++;
+                }
+
+                Integer evaluationResult = getInteger(element, calculationValuesInTheCorrectOrder);
+
+                if (evaluationResult != null){
+                    collectionStack.push(evaluationResult);
+                }
+            }
+        }
+
+        // We expect to be left with the final result
+        if (!collectionStack.isEmpty()) return collectionStack.pop();
+        return 0;
+    }
+
+    private static Integer getInteger(String element, Integer[] values) {
+        Integer previousVal = null;
+
+        for (Integer val: values){
+
+            if (previousVal == null) {
+                previousVal = val;
+                continue;
+            }
+
+            switch (element) {
+                case "-" -> {
+                    previousVal -= val;
+                }
+                case "+" -> {
+                    previousVal += val;
+                }
+                case "*" -> {
+                    previousVal *= val;
+                }
+                case "/" -> {
+                    previousVal /= val;
+                }
+            }
+        }
+        return previousVal;
+    }
+
+    public static void main(String[] args) {
+
+        ReversePolishNotation reversePolishNotation = new ReversePolishNotation();
+
+        /*
+            Expect 5
+         */
+        System.out.println(reversePolishNotation.evalRPN(new String[]{"1","2","+","3","*","4","-"}));
+
+        /*
+            Expect 6
+         */
+        System.out.println(reversePolishNotation.evalRPN(new String[]{"4","13","5","/","+"}));
+
+        /*
+            Expect 2
+                4, 2, 5 * + 1 3 2 * + /
+         */
+        System.out.println(reversePolishNotation.evalRPN(new String[]{"4","2","5","*","+","1","3","2","*","+","/"}));
+    }
+
+}
