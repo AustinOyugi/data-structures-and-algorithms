7d8c3ed25121ce4e047f8d4c6a8cde6e45a508d9
Austin Oyugi <austinoyugi@gmail.com>
2024-11-27 04:08:47 +0300

    Create Matrix2DSearch.java

---
+++ src/binarysearch/Matrix2DSearch.java
@@ -0,0 +1,29 @@
+package binarysearch;
+
+import static binarysearch.BinarySearch.search;
+
+class Matrix2DSearch {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+
+        for (int[] arrayJ : matrix) {
+
+            int lowestValue = arrayJ[0];
+            int highestValue = arrayJ[arrayJ.length - 1];
+
+            //Check if value exists
+            if (target == lowestValue || target == highestValue) {
+                return true;
+            }
+
+            if (target > lowestValue && target < highestValue) {
+                return search(arrayJ, target) != -1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        //Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
+    }
+}
